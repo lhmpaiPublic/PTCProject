409a7a88236afc56951fec30f52d1fb53466e989
Lee Hyunmu <vawing21@naver.com>
2023-03-30 19:40:02 +0900

    add Recip Import

---
+++ NotchingGradeInsp_NotUseDLL/RecipImportDlg.cpp
@@ -0,0 +1,129 @@
+﻿// CRecipeSelectDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "NotchingGradeInsp.h"
+#include "afxdialogex.h"
+#include "Win32File.h"
+#include "RecipImportDlg.h"
+#include "LogDisplayDlg.h"
+#include "GlobalData.h"
+
+// CRecipeSelectDlg 대화 상자
+
+IMPLEMENT_DYNAMIC(CRecipeImportDlg, CDialogEx)
+
+CRecipeImportDlg::CRecipeImportDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DLG_RECIPEIMPORT, pParent)
+	, m_strRecipeName(_T(""))
+	, m_strRecipeMemo(_T(""))
+{
+
+}
+
+CRecipeImportDlg::~CRecipeImportDlg()
+{
+}
+
+void CRecipeImportDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	//DDX_Control(pDX, IDC_LIST1, m_ListRecipeName);
+	DDX_CBString(pDX, IDC_COMBO_RECIPE_NAME, m_strRecipeName);
+	DDX_Text(pDX, IDC_EDIT_RECIPE_MEMO, m_strRecipeMemo);
+	DDX_Control(pDX, IDC_COMBO_RECIPE_NAME, m_RecipeNameCtrl);
+}
+
+
+BEGIN_MESSAGE_MAP(CRecipeImportDlg, CDialogEx)
+	ON_BN_CLICKED(IDOK, &CRecipeImportDlg::OnBnClickedOk)
+	ON_BN_CLICKED(IDC_BUT_RECIPEDELETE, &CRecipeImportDlg::OnBnClickedButRecipedelete)
+	ON_CBN_SELCHANGE(IDC_COMBO_RECIPE_NAME, &CRecipeImportDlg::OnCbnSelchangeComboRecipeName)
+END_MESSAGE_MAP()
+
+
+// CRecipeSelectDlg 메시지 처리기
+
+
+void CRecipeImportDlg::OnBnClickedOk()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	UpdateData();
+	//메모 기본 텍스트 쓰기
+	m_strRecipeMemo = m_strRecipeName;
+	CDialogEx::OnOK();
+}
+
+void CRecipeImportDlg::LoadRecipeFileList()
+{
+	//로드된 레시피명 정보
+	CStringList strRecipeList;
+	strRecipeList.RemoveAll();
+
+	//레시피 파일이 들어있는 경로 설정
+	CString strRecipeDir;
+	strRecipeDir.Format(_T("%s\\Recipe\\"), AprData.m_strDataPath);
+
+	CLogDisplayDlg::gInst()->AddLogDisplayMessage("RecipeListLoad 경로 : " + strRecipeDir);
+
+	//경로에 있는 레시피 파일 목록을 가져온다.
+	CWin32File winFile;
+	winFile.GetFileList(strRecipeDir, strRecipeList);
+
+	//가져온 레시피 파일의 목록을 콤보박스에 추가한다.
+	POSITION pos;
+	CString strTemp;
+	for (pos = strRecipeList.GetHeadPosition(); pos != NULL; ) {
+		strTemp = strRecipeList.GetNext(pos);
+		strTemp.Replace(_T(".ini"), _T(""));
+		m_RecipeNameCtrl.AddString(strTemp);
+	}
+	m_RecipeNameCtrl.SetCurSel(0);
+
+	CString str;
+	m_RecipeNameCtrl.GetLBText(0, str);
+	//메모 기본 텍스트 쓰기
+	m_strRecipeName = m_strRecipeMemo = str;
+	UpdateData(FALSE);
+}
+
+BOOL CRecipeImportDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// TODO:  여기에 추가 초기화 작업을 추가합니다.
+	LoadRecipeFileList();
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+				  // 예외: OCX 속성 페이지는 FALSE를 반환해야 합니다.
+}
+
+
+void CRecipeImportDlg::OnBnClickedButRecipedelete()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	UpdateData();
+	CString deleteFileName;
+	deleteFileName.Format(_T("%s\\Recipe\\%s.ini"), AprData.m_strDataPath, m_strRecipeName);
+	if (DeleteFile(deleteFileName) == FALSE)
+	{
+		MessageBox(_T("파일 삭제 실패"));
+	}
+	else
+	{
+		m_RecipeNameCtrl.ResetContent();
+		LoadRecipeFileList();
+	}
+}
+
+
+void CRecipeImportDlg::OnCbnSelchangeComboRecipeName()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	CString str;
+	int index = m_RecipeNameCtrl.GetCurSel();
+	m_RecipeNameCtrl.GetLBText(m_RecipeNameCtrl.GetCurSel(), str);
+	//메모 기본 텍스트 쓰기
+	m_strRecipeName = m_strRecipeMemo = str;
+	UpdateData(FALSE);
+}
---
+++ NotchingGradeInsp_NotUseDLL/RecipImportDlg.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+
+// CRecipeImportDlg 대화 상자
+
+class CRecipeImportDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CRecipeImportDlg)
+
+public:
+	CRecipeImportDlg(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CRecipeImportDlg();
+
+	// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DLG_RECIPEIMPORT};
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+
+	CListBox m_ListRecipeName;
+	virtual BOOL OnInitDialog();
+
+private:
+	//레시피 파일 목록을 얻는다.
+	void LoadRecipeFileList();
+public:
+	//선택된 레시피명
+	CString m_strRecipeName;
+	//선택된 레시피의 메모
+	CString m_strRecipeMemo;
+	//레시피 명 콤보박스 컨트롤 제어 객체
+	CComboBox m_RecipeNameCtrl;
+	afx_msg void OnBnClickedButRecipedelete();
+	afx_msg void OnCbnSelchangeComboRecipeName();
+};
