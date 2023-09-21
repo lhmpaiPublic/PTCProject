48fdae99c80bf557d4b4c3b5688688cd200aa1a2
Lee Hyunmu <vawing21@naver.com>
2023-09-21 17:56:42 +0900

    DIO Trigger 신호 처리 데모 용 값 0 프로그램 version 2.0

--- NotchingGradeInsp/pch.h
+++ NotchingGradeInsp/pch.h
@@ -27,11 +27,11 @@ using namespace Gdiplus;
 #pragma comment(linker, "/HEAP:4000000000")
 
 //DIO 보드넘버 : 현장 pc 0 vs DEMO pc 1
-#define DIO_BOARD_NO 1
+#define DIO_BOARD_NO 0
 
 //SPC 객체 소스에서 컴파일 여부 결정
 //SPC+ 생성을 위한 플래그
-//#define SPCPLUS_CREATE
+#define SPCPLUS_CREATE
 
 // GMLV Line용 - 조명 컨트롤러 1개 사용 시 Error Skip
 //#define GMLV_LIGHT_SKIP
