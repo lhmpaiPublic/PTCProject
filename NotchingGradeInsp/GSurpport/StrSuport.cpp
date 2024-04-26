5921c95a91ebfbca9d403444af289f9935198074
Lee Hyunmu <vawing21@naver.com>
2024-04-26 11:51:35 +0900

    Str Suport : plc data log 데이터 만들가 함수 수정

--- NotchingGradeInsp/GSurpport/StrSuport.cpp
+++ NotchingGradeInsp/GSurpport/StrSuport.cpp
@@ -131,7 +131,7 @@ CString CStrSuport::ChangbytetohexTab(BYTE* byt, int len, int offset, int tablen
 	{
 		if ((i + 1) % tablen == 1)
 		{
-			temp.Format("%d : ", (i+ offset));
+			temp.Format("0x%02X : ", (i + offset));
 			rest += temp;
 		}
 		temp.Format("%02X", byt[i]);
@@ -157,7 +157,7 @@ CString CStrSuport::ChangshorttohexTab(short* data, int len, int offset, int tab
 	{
 		if ((i+1) % tablen == 1)
 		{
-			temp.Format("%d : ", (i + offset));
+			temp.Format("0x%02X : ", ((i * sizeof(short)) + offset));
 			rest += temp;
 		}
 		temp.Format("%02X", ((data[i])>>8)&0xff);
@@ -185,7 +185,7 @@ CString CStrSuport::ChanginttohexTab(int* data, int len, int offset, int tablen)
 	{
 		if ((i + 1) % tablen == 1)
 		{
-			temp.Format("%d : ", (i + offset));
+			temp.Format("0x%02X : ", ((i * sizeof(int)) + offset));
 			rest += temp;
 		}
 		temp.Format("%02X", ((data[i]) >> 24) & 0xff);
